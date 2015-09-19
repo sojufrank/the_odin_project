f10743e9b5b1ce3677d58d2b96f52b8eede79c95
sojufrank <frankyiyi@gmail.com>
2015-09-19 03:36:28 -0700

    added sketch project

---
+++ sketch/sketch.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+for (i=0; i<4; i++){
+    $('table').append('<tr><td></td><td></td><td></td><td></td></tr>');	   
+}
+
+$("td").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "yellow");
+});
+
+test = function(){
+x= prompt("how many grid blocks? try 1-50");
+    $("tr").remove();    
+    
+    for (i=0; i<x; i++){
+    $('table').append('<tr></tr>');
+    }
+        for(i=0; i<x; i++){
+        $('tr').append('<td></td>');
+        }
+
+
+$("td").hover(function(){
+    $(this).css("background-color", "pink");
+    }, function(){
+    $(this).css("background-color", "yellow");
+});
+}
+
+$('button').click(test);
+
+});
